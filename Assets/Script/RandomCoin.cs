d28ce4a83c7da2365eb2c20b2da37434ea4274dc
haruka20251 <h10m3d5contrail@gmail.com>
2025-02-26 04:14:45 +0900

    コインの回転を追加しました
    コインの追加と回転の設定

---
+++ Assets/Script/RandomCoin.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCoin : MonoBehaviour
+{
+    public GameObject coin;
+    private float randomX = 17;//ê∂ê¨ïùX
+    private float randomZ = 28;//ê∂ê¨ïùZ
+    private float PosY = 0.444f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
